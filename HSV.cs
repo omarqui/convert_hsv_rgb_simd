292746c8facf6ce2b845baba2124ddd2b4fb49a5
omarqui <omarquiaybar@gmail.com>
2021-03-04 01:28:07 -0400

    implement HSV to RGB with simd

--- HSV.cs
+++ HSV.cs
@@ -1,15 +1,18 @@
 using System;
+using System.Numerics;
+using System.Diagnostics;
+
 namespace hsv_rgb_simd
 {
     public class HSV
-    {        
-        public const double HUE_DIVISOR = 60;
+    {
+        public const float HUE_DIVISOR = 60;
 
         public Int16 Hue { get; set; }
-        public Double Saturation { get; set; }
-        public Double Value { get; set; }
+        public float Saturation { get; set; }
+        public float Value { get; set; }
 
-        public HSV(Int16 h, Double s, Double v)
+        public HSV(Int16 h, float s, float v)
         {
             Hue = h;
             Saturation = s;
@@ -20,15 +23,139 @@ namespace hsv_rgb_simd
         {
         }
 
-        public RGB toRGB()
-        {            
-            double choma = Value * Saturation;                        
-            double intermediate = choma * (1 - Math.Abs(((Hue / HUE_DIVISOR) % 2) - 1));
+        public static RGB[] ToRGBSimd(HSV[] hsvArray)
+        {
+            int totalConvertions = hsvArray.Length;
+            float[] hueArray = new float[totalConvertions];
+            float[] saturationArray = new float[totalConvertions];
+            float[] valueArray = new float[totalConvertions];
 
-            int quotient60 = (int)Math.Round((Hue / HUE_DIVISOR),0);
+            for (int i = 0; i < totalConvertions; i++)
+            {
+                hueArray[i] = hsvArray[i].Hue;
+                saturationArray[i] = hsvArray[i].Saturation;
+                valueArray[i] = hsvArray[i].Value;
+            }
+
+            int vectorSize = Vector<float>.Count;
+            float[] redHelperArray = new float[totalConvertions];
+            float[] greenHelperArray = new float[totalConvertions];
+            float[] blueHelperArray = new float[totalConvertions];
+            float[] redArray = new float[totalConvertions];
+            float[] greenArray = new float[totalConvertions];
+            float[] blueArray = new float[totalConvertions];
+
+            Vector<float> maxRgbVector = new Vector<float>(RGB.MAX_RGB_VALUE);
+            Vector<float> hueDivisorVector = new Vector<float>(HUE_DIVISOR);
+
+            Vector<float> hueVector;
+            Vector<float> valueVector;
+            Vector<float> saturationVector;
+            Vector<float> cromaVector;
+            Vector<float> hueModVector;
+            Vector<float> chomaAlfaVector;
+            Vector<float> intermediateVector;
+            Vector<float> quotient60Vector;
+
+            Vector<float> factorVector;
+            Vector<float> redHelperVector;
+            Vector<float> greenHelperVector;
+            Vector<float> blueHelperVector;
+
+            Vector<float> redVector;
+            Vector<float> greenVector;
+            Vector<float> blueVector;
+
+            RGB[] rgbArray = new RGB[totalConvertions];
+            for (int i = 0; i < totalConvertions; i += vectorSize)
+            {
+                hueVector = new Vector<float>(hueArray, i);
+                valueVector = new Vector<float>(valueArray, i);
+                saturationVector = new Vector<float>(saturationArray, i);
+
+                cromaVector = valueVector * saturationVector;
+                hueDivisorVector = new Vector<float>(HUE_DIVISOR);
+                hueModVector = ModVector(hueVector, hueDivisorVector);
+
+                chomaAlfaVector = Vector<float>.One - Vector.Abs<float>(hueModVector - Vector<float>.One);
+                intermediateVector = cromaVector * chomaAlfaVector;
+                quotient60Vector = hueVector / hueDivisorVector;
+                redHelperVector = new Vector<float>();
+                greenHelperVector = new Vector<float>();
+                blueHelperVector = new Vector<float>();
+
+                for (int j = 0; j < vectorSize; j++)
+                {
+                    int k = i + j;
+                    switch ((int)Math.Round(quotient60Vector[j], 0))
+                    {
+                        case 0:
+                            redHelperArray[k] = cromaVector[j];
+                            greenHelperArray[k] = intermediateVector[j];
+                            blueHelperArray[k] = 0;
+                            break;
+                        case 1:
+                            redHelperArray[k] = intermediateVector[j];
+                            greenHelperArray[k] = cromaVector[j];
+                            blueHelperArray[k] = 0;
+                            break;
+                        case 2:
+                            redHelperArray[k] = 0;
+                            greenHelperArray[k] = cromaVector[j];
+                            blueHelperArray[k] = intermediateVector[j];
+                            break;
+                        case 3:
+                            redHelperArray[k] = 0;
+                            greenHelperArray[k] = intermediateVector[j];
+                            blueHelperArray[k] = cromaVector[j];
+                            break;
+                        case 4:
+                            redHelperArray[k] = intermediateVector[j];
+                            greenHelperArray[k] = 0;
+                            blueHelperArray[k] = cromaVector[j];
+                            break;
+                        case 5:
+                            redHelperArray[k] = cromaVector[j];
+                            greenHelperArray[k] = 0;
+                            blueHelperArray[k] = intermediateVector[j];
+                            break;
+                    }
+                }
+
+                factorVector = valueVector - cromaVector;
+                maxRgbVector = new Vector<float>(RGB.MAX_RGB_VALUE);
+                redVector = (redHelperVector + factorVector) * maxRgbVector;
+                greenVector = (greenHelperVector + factorVector) * maxRgbVector;
+                blueVector = (blueHelperVector + factorVector) * maxRgbVector;
+
+                redVector.CopyTo(redArray, i);
+                greenVector.CopyTo(greenArray, i);
+                blueVector.CopyTo(blueArray, i);
+            }
+
+            for (int i = 0; i < redArray.Length; i++)
+            {
+                rgbArray[i] = new RGB((short)redArray[i], (short)greenArray[i], (short)blueArray[i]);
+            }
+
+            return rgbArray;
+        }
+
+        private static Vector<float> ModVector(Vector<float> hueVector, Vector<float> divisor)
+        {
+            Vector<float> disition = hueVector / divisor;
+            return (disition - Vector.ConvertToSingle(Vector.ConvertToInt32(disition))) * divisor;
+        }        
+
+        public RGB toRGB()
+        {
+            float choma = Value * Saturation;
+            float intermediate = choma * (1 - Math.Abs(((Hue / HUE_DIVISOR) % 2) - 1));
+
+            int quotient60 = (int)Math.Round((Hue / HUE_DIVISOR), 0);
             if (quotient60 >= 6) return null;
 
-            double redHelper = 0, greenHelper = 0, blueHelper = 0;
+            float redHelper = 0, greenHelper = 0, blueHelper = 0;
             switch (quotient60)
             {
                 case 0:
@@ -63,12 +190,12 @@ namespace hsv_rgb_simd
                     break;
             }
 
-            double factor = Value - choma;
+            float factor = Value - choma;
             short red, green, blue;
-            red = (short) Math.Round(((redHelper + factor) * RGB.MAX_RGB_VALUE),0);
-            green = (short) Math.Round(((greenHelper + factor) * RGB.MAX_RGB_VALUE));
-            blue = (short) Math.Round(((blueHelper + factor) * RGB.MAX_RGB_VALUE));
-            return new RGB(red,green,blue);
+            red = (short)Math.Round(((redHelper + factor) * RGB.MAX_RGB_VALUE), 0);
+            green = (short)Math.Round(((greenHelper + factor) * RGB.MAX_RGB_VALUE));
+            blue = (short)Math.Round(((blueHelper + factor) * RGB.MAX_RGB_VALUE));
+            return new RGB(red, green, blue);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace hsv_rgb_simd
 {
@@ -6,17 +7,79 @@ namespace hsv_rgb_simd
     {
         static void Main(string[] args)
         {
-            HSV colorHsv = new HSV(270, 0.5, 0.5);
-            RGB colorRgb = colorHsv.toRGB();
-            HSV newHsv = colorRgb.toHSV();
+            Stopwatch sw = new Stopwatch();
+            HSV[] list = GenerateHsv(10000000);
+
             
+            // sw.Start();
+            // RGB[] rgbArray = new RGB[list.Length];
+            // for(int i = 0; i < list.Length; i++)
+            // {
+            //     rgbArray[i] = list[i].toRGB();
+            // }            
+            // sw.Stop();
+
+            // Console.WriteLine("1 - Elapsed={0}", sw.Elapsed);
+
+            
+            // sw.Reset();
+            sw.Start();            
+            RGB[] rgbArray2 = HSV.ToRGBSimd(list);
+
+            sw.Stop();
+
+            Console.WriteLine("2 - Elapsed={0}", sw.Elapsed);
+            // sw.Reset();
+
+        }
+
+        private static void prueba1()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            HSV colorHsv = new HSV(270, 0.5f, 0.5f);
+            RGB colorRgb = colorHsv.toRGB();
+            // HSV newHsv = colorRgb.toHSV();
+
             Console.WriteLine(colorRgb.Red);
             Console.WriteLine(colorRgb.Green);
             Console.WriteLine(colorRgb.Blue);
+            sw.Stop();
 
-            Console.WriteLine(newHsv.Hue);
-            Console.WriteLine(newHsv.Saturation);
-            Console.WriteLine(newHsv.Value);
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+
+            sw.Start();
+            HSV[] hsvArray = new HSV[]{
+                colorHsv,
+                colorHsv,
+                colorHsv,
+                colorHsv
+            };
+            RGB[] rgbArray = HSV.ToRGBSimd(hsvArray);
+            RGB colorRgb2 = rgbArray[0];
+            // HSV newHsv = colorRgb.toHSV();
+
+            Console.WriteLine(colorRgb2.Red);
+            Console.WriteLine(colorRgb2.Green);
+            Console.WriteLine(colorRgb2.Blue);
+            // Console.WriteLine(newHsv.Hue);
+            // Console.WriteLine(newHsv.Saturation);
+            // Console.WriteLine(newHsv.Value);
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+        }
+
+        static HSV[] GenerateHsv(int cant)
+        {
+            HSV colorHsv = new HSV(270, 0.5f, 0.5f);
+            HSV[] result = new HSV[cant];
+            for (int i = 0; i < cant; i++)
+            {
+                result[i] = colorHsv;
+            }
+            return result;
         }
     }
 }
